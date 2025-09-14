536506f4242e0fdcf6eb696fd9b9a22897eb69f0
netsugo <22271060+netsugo@users.noreply.github.com>
2025-09-14 06:36:51 +0000

    update tag rule

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -4,12 +4,15 @@ set -euo pipefail
 USERNAME="github-actions[bot]"
 EMAIL="41898282+github-actions[bot]@users.noreply.github.com"
 TAG_NAME=$1
+LIB_VERSION="0.1"
 
 # commit
 git config --local user.name "${USERNAME}"
 git config --local user.email "${EMAIL}"
 git add .
 git commit -m "Update Twemoji to ${TAG_NAME}"
+
+TAG=$(echo "${LIB_VERSION}-${TAG_NAME}" | sed -e 's/v//g')
 
 # push
 if [ $? -eq 0 ]; then
