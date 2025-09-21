7e5befe0f7bf3565136b721c8e5482a50c225749
netsugo <22271060+netsugo@users.noreply.github.com>
2025-09-21 15:55:02 +0900

    fixed compose dependencies

--- twemoji/build.gradle.kts
+++ twemoji/build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.maven.publish)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -21,11 +25,37 @@ android {
         }
     }
 
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlin.get()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     publishing {
         singleVariant("release") {
             withSourcesJar()
         }
     }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.fromTarget("17")
+    }
+}
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
 }
 
 publishing {
